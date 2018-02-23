efeeeb81592eb162c28a4ea20b8708cd7a5d7b24
Justin <rapidmon17@gmail.com>
2018-02-22 22:17:51 -0500

    Updated for heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
+var {Todo} = require('./models/Todo');
 var {User} = require('./models/user');
 
 var app = express();
